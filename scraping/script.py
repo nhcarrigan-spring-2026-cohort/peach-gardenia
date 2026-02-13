2668908db9923941872ac0d0828fac8826a9ed6a
andiedoescode <pang.andrea@gmail.com>
2026-02-13 17:16:31 -0500

    Added Selenium. Item name now retrieves.

--- scraping/script.py
+++ scraping/script.py
@@ -1,73 +1,104 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import requests
 from bs4 import BeautifulSoup
 
+def get_wishlist_items(
+    url: str,
+    # headless: True,
+    timeout: int = 20
+):
 
-def get_wishlist_items():
+    # Setting Selenium browser options
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
-    headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    }
+    driver = webdriver.Chrome(options=options)
 
-    ### Using local file to test
-    # with open("Amazon.htm", "r", encoding="utf-8") as file:
-    #     page = file.read()
-        
-    # soup = BeautifulSoup(page, "html.parser")
+    driver.get(url)
 
-    ### Using live URL
-    url = "https://www.amazon.com/hz/wishlist/ls/YW88P7BXG3A0?ref_=wl_fv_le"
-    page = requests.get(url, headers=headers)
-    print (page.text[:2000])
+    try:
+        WebDriverWait(driver, timeout). until(
+            EC.presence_of_element_located((By.ID, "g-items"))
+        )
+    except:
+        pass
 
-    soup = BeautifulSoup(page.content, "html.parser")
+    html = driver.page_source
+    driver.quit()
 
-    results = soup.find(id="g-items")
+    # BeautifulSoup parsing
+    soup = BeautifulSoup(html, "html.parser")
 
-    # if results is not None:
-    #     wishlist_items = results.find_all("li", class_=lambda text: "g-item-sortable")
-
-    wishlist_items = results.find_all("li", class_=lambda text: "g-item-sortable")
+    wishlist_items = soup.select("li.g-item-sortable")
 
     res = []
 
-
     for wishlist_item in wishlist_items:
-        item = wishlist_item.find(
-            "a", 
-            id=lambda text: text is not None and "itemName_" in text,
-            class_="a-link-normal", 
-        )
 
-        if item and item.get("title"):
-            item_name = item.get("title")
-            item_url = f"https://www.amazon.com/{item.get("href")}"
-            image_url = wishlist_item.find("img").get("src")
+        item_name = None
 
-            item_needed = wishlist_item.find("span", id=lambda text: text is not None and "itemRequested_" in text)
-            item_received = wishlist_item.find("span", id=lambda text: text is not None and "itemPurchased_" in text)
-            item_priority = wishlist_item.find("span", id=lambda text: text is not None and "itemPriorityLabel_" in text)
+        # Checking for main product link for each wishlist item
+        item_block = wishlist_item.select_one("a.a-link-normal[id^='itemName_']")
+        
+        # If no match then skip and continue
+        if not item_block:
+            continue
 
-            item_add_string = wishlist_item.find("span", id=lambda text: text is not None and "itemAddedDate_" in text)
-            # item_add_date = wishlist_item.find("span", id=lambda text: text is not None and "itemAddedDate_" in text).text.strip()[11:]
+        # If match exists, then get title attr otherwise get the visible text
+        if item_block.get("title"):
+            item_name = item_block.get("title").strip()
+        else:
+            text = item_block.get_text(strip=True)
+            if text:
+                item_name = text
 
-            if item_add_string:
-                item_add = item_add_string.text.strip()
-                item_add_date = item_add[11:]
+        if item_name:
+            data = {
+                "item_name": item_name,
+            }
+        
+            res.append(data)
 
-                data = {
-                    'item_name': item_name,
-                    'item_priority': item_priority.text.strip(),
-                    'item_add_date': item_add_date,
-                    'item_needed': item_needed.text.strip(),
-                    'item_received': item_received.text.strip(),
-                    'item_url': item_url,
-                    'item_image_url': image_url
-                }
+        # href = item_block.findElement(By.tagName("href"))
 
-                res.append(data)
+        # if item and item.get("title"):
+        #     item_name = item.get("title")
+        #     item_url = f"https://www.amazon.com/{item.get("href")}"
+        #     image_url = wishlist_item.find("img").get("src")
+
+        #     item_needed = wishlist_item.find("span", id=lambda text: text is not None and "itemRequested_" in text)
+        #     item_received = wishlist_item.find("span", id=lambda text: text is not None and "itemPurchased_" in text)
+        #     item_priority = wishlist_item.find("span", id=lambda text: text is not None and "itemPriorityLabel_" in text)
+
+        #     item_add_string = wishlist_item.find("span", id=lambda text: text is not None and "itemAddedDate_" in text)
+        #     # item_add_date = wishlist_item.find("span", id=lambda text: text is not None and "itemAddedDate_" in text).text.strip()[11:]
+
+        #     if item_add_string:
+        #         item_add = item_add_string.text.strip()
+        #         item_add_date = item_add[11:]
+
+        #         data = {
+        #             'item_name': item_name,
+        #             'item_priority': item_priority.text.strip(),
+        #             'item_add_date': item_add_date,
+        #             'item_needed': item_needed.text.strip(),
+        #             'item_received': item_received.text.strip(),
+        #             'item_url': item_url,
+        #             'item_image_url': image_url
+        #         }
+
+        #         res.append(data)
+
 
     print(res)
     # return res
 
-get_wishlist_items()
+live_url = "https://www.amazon.com/hz/wishlist/ls/YW88P7BXG3A0?ref_=wl_fv_le"
+get_wishlist_items(live_url)
